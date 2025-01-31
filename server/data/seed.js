aabe595861fac83c5dc0e251784e4b441e3e2a56
Doug Scheible <doug_scheible@yahoo.com>
2025-01-31 17:49:07 -0600

    starting to use REACT over VUE

---
+++ server/data/seed.js
@@ -0,0 +1,5 @@
+// Since I've seeded the DB my self I skipped this part. If you want to find out what the
+// seed.js file looks like and the script it involves to drop and start a new db with
+// a couple of entries it is on "Step 2 - Seeding MongoDB database with data"
+// https://www.youtube.com/watch?v=a86FWdOzSak&list=PLCk2SWeTHqQiinxux9GJFF8YDhOupddpr&index=2
+// at the 4:00 mark
